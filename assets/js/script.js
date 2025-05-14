f21f6f5b6ea91264d66e22c4cc1893e236c0467f
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-05-14 11:43:01 +0100

    make a POST REQUEST

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,45 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e)); // e is a reference to the event.
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json()
+
+    if (response.ok) {
+        displayErrors(data); //calls function (line 28)
+    } else {
+        throw new Error(data.error);
+    }
+};
+
+function displayErrors(data) {
+    let modalHeading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors>No errors reported</div>`
+    } else {
+        results = `<div>Total Errors: <span class="error_counts">${data.total_errors}</span></div>`
+        for (let error of data.error_list) { //content for error.
+            results += `<div>At line <span class="line">${error.line}</span>,`;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+    document.getElementById("resultsModalTitle").innerText = modalHeading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show(); // shows the alert modal that drops down.
+}
 
 async function getStatus(e) { //waits fro promise to come true
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -16,7 +55,7 @@ async function getStatus(e) { //waits fro promise to come true
     } else {
         throw new Error(data.error);
     }
-}
+};
 
 function displayStatus(data) {
     let modalHeading = "API Key Status";
